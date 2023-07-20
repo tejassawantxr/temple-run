6461e1a3ef7e63218037f05855f8aa29df4cc075
Tejas <sawanttejasar@gmail.com>
2023-07-21 01:25:51 +0530

    simulate gravity and allow player to move left and right by using keyboard keys

--- Hunt-Points/Assets/Scripts/PlayerMovement.cs
+++ Hunt-Points/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float speed = 5.0f;
+    
+    Vector3 moveVector;
+    float gravity = -9.81f;
 
     CharacterController controller;
     void Start()
@@ -15,6 +18,16 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        controller.Move((Vector3.forward * speed) * Time.deltaTime); //frame rate independent
+        if(controller.isGrounded){
+            gravity=-0.5f;
+        }else{
+            gravity=-9.81f;
+        }
+        // moveVector = Vector3.zero;
+        moveVector.x = Input.GetAxisRaw("Horizontal") * speed;
+        moveVector.y = gravity;
+        moveVector.z = speed;
+
+        controller.Move(moveVector * Time.deltaTime); //frame rate independent
     }
 }
