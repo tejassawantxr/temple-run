e8ed3343229aecb8084bca5b8f744586d7b3c461
Tejas <sawanttejasar@gmail.com>
2023-07-21 12:26:19 +0530

    centered the camera and restricted the movement of the camera along y axis

--- Hunt-Points/Assets/Scripts/CameraMovement.cs
+++ Hunt-Points/Assets/Scripts/CameraMovement.cs
@@ -7,16 +7,21 @@ public class CameraMovement : MonoBehaviour
     // Start is called before the first frame update
     Transform followAt;
     Vector3 offset;
+    Vector3 moveVector;
 
     void Start()
     {
-        followAt = GameObject.FindGameObjectWithTag("Player").transform;
-        offset = transform.position - followAt.position;
+        followAt = GameObject.FindGameObjectWithTag("Player").transform;  //camera to follow the player
+        offset = transform.position - followAt.position;   //distance between camera and player  
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = followAt.position + offset;
+        moveVector = followAt.position + offset; //follow the player by keeping offset distance in between camera and player
+        moveVector.x = 0;  //camera should be at the center
+        moveVector.y = Mathf.Clamp(moveVector.y, 3, 5); 
+        
+        transform.position = moveVector;  
     }
 }
