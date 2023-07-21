56656cf22926121fc8a7bc932888c1e21db84b95
Tejas <sawanttejasar@gmail.com>
2023-07-22 02:49:45 +0530

    replaced transform.translate() using CharacterController

--- Hunt-Points/Assets/Scripts/PlayerMovement.cs
+++ Hunt-Points/Assets/Scripts/PlayerMovement.cs
@@ -6,8 +6,12 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5.0f; 
     [SerializeField] float rotationSpeed = 5.0f;
+
+    CharacterController characterController;
+
     void Start()
     {
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -18,13 +22,13 @@ public class PlayerMovement : MonoBehaviour
        float zValue = Input.GetAxis("Vertical");
 
        Vector3 movementDirection = new Vector3(xValue, yValue, zValue);
-       movementDirection.Normalize();
-       transform.Translate(movementDirection * moveSpeed * Time.deltaTime, Space.World);
-       
+       movementDirection.Normalize();  //diagonally character moves faster compared to moving in x and y direction. So x, y or diagonally the character will move by 1 unit only.
+       characterController.SimpleMove(movementDirection * moveSpeed); //Build-in frame rate independent
+
        if(movementDirection != Vector3.zero){
-            // transform.forward = movementDirection;
-            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation,toRotation, rotationSpeed * Time.deltaTime);
+            //Characters smooth rotation to the direction along with the face and body
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up); //Calulates rotation value of how much to rotate
+            transform.rotation = Quaternion.RotateTowards(transform.rotation,toRotation, rotationSpeed * Time.deltaTime); //smooth rotation of the characters body to the specific directions
        }
     }
 }
