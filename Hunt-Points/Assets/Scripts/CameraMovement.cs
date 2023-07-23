4a8eff695c1230bbe991669e261032d03eeec485
Tejas <sawanttejasar@gmail.com>
2023-07-23 21:15:20 +0530

    CameraMovement fixed

--- Hunt-Points/Assets/Scripts/CameraMovement.cs
+++ Hunt-Points/Assets/Scripts/CameraMovement.cs
@@ -4,24 +4,18 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    Transform lookAt;
+    Vector3 startOffSet;
     // Start is called before the first frame update
-    Transform followAt;
-    Vector3 offset;
-    Vector3 moveVector;
-
     void Start()
     {
-        followAt = GameObject.FindGameObjectWithTag("Player").transform;  //camera to follow the player
-        offset = transform.position - followAt.position;   //distance between camera and player  
+        lookAt = GameObject.FindGameObjectWithTag("Player").transform;
+        startOffSet = transform.position - lookAt.position;   //distance between camera and player  
     }
 
     // Update is called once per frame
     void Update()
     {
-        moveVector = followAt.position + offset; //follow the player by keeping offset distance in between camera and player
-        moveVector.x = 0;  //camera should be at the center
-        moveVector.y = Mathf.Clamp(moveVector.y, 5, 7); 
-
-        transform.position = moveVector;  
+        transform.position = lookAt.position + startOffSet;  //follow the player by keeping offset distance in between camera and player
     }
 }
