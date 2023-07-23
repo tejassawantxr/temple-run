7b92fbe8adb01a36f98f6058d21843df18a34aad
Tejas <sawanttejasar@gmail.com>
2023-07-24 05:03:59 +0530

    SceneController created

--- Hunt-Points/Assets/Scripts/SceneController.cs
+++ Hunt-Points/Assets/Scripts/SceneController.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class SceneController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   public void MainMenu(){
+    SceneManager.LoadScene("MainMenu");
+   }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   public void StartGame(){
+    SceneManager.LoadScene("Game");
+   }
+
+   public void QuitGame(){
+    Application.Quit();
+   }
 }
